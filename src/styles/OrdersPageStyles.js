2ffe176ae1e66c89d10dc06f018b5365dc18f3bd
wsYYnb3 <danielisraeli033@gmail.com>
2023-12-23 19:29:02 +0100

    no orders styling improved

--- src/styles/OrdersPageStyles.js
+++ src/styles/OrdersPageStyles.js
@@ -1,6 +1,30 @@
 import { Card } from "react-bootstrap";
 import styled from "styled-components";
+import { FaRegSadTear } from "react-icons/fa";
 
+export const NoOrdersContainer = styled.div`
+  text-align: center;
+  padding: 50px 20px;
+  margin-top: 50px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background-color: #f8f8f8;
+`;
+
+export const NoOrdersIcon = styled(FaRegSadTear)`
+  color: #6c757d;
+  font-size: 48px;
+  margin-bottom: 20px;
+`;
+
+export const NoOrdersText = styled.h3`
+  color: #333;
+  margin-bottom: 15px;
+`;
+
+export const NoOrdersSubText = styled.p`
+  color: #666;
+`;
 export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
